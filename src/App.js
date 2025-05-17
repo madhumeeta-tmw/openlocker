1f578365d0dcc141639f25154fb3f92d001889ce
madhumeeta-tmw-dev <madhumeeta.s@themailwhale.in>
2025-05-17 11:25:11 +0530

    open locker configured git ignore and other dependencies

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import LockerControl from './components/LockerControl';
+
+function App() {
+  return (
+    <div className="App">
+      <LockerControl />
+    </div>
+  );
+}
+
+export default App;
